7a67757189039afd5d599e389e76fc7123494aaf
Maria Vanyushkina <m199178@mail.ru>
2022-09-29 23:13:22 +0300

    first commit variables

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,35 @@
 function solveEquation(a, b, c) {
+  'use strict';
   let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+  let d = b**2-4*a*c;
+    if (d === 0) {
+      arr = [-b/(2*a)];
+    } else if (d > 0) {
+      arr = [(-b + Math.sqrt(d) )/(2*a), (-b - Math.sqrt(d) )/(2*a)];
+    } else {
+      arr = [];
+    }
+  return arr; 
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
+  'use strict';
   let totalAmount;
-
-  // код для задачи №2 писать здесь
-
+    if (isNaN(percent) == true) {
+      totalAmount = 'Параметр "Процентная ставка" содержит неправильное значение ' + percent;
+    } else if (isNaN(contribution) == true) {
+      totalAmount = 'Параметр "Начальный взнос" содержит неправильное значение ' + contribution;
+    } else if (isNaN(amount) == true) {
+      totalAmount = 'Параметр "Общая стоимость" содержит неправильное значение ' + amount;
+    } else {
+      let S = amount - contribution;
+      let dateNow = new Date();
+      let n = Math.trunc((date - dateNow)/2592000000);
+      let P = 1/12 * (percent/100);
+      let monthAmount = S*(P + (P/(((1 + P)**n) - 1)));
+      totalAmount = Number((n*monthAmount).toFixed(2));
+      console.log(totalAmount);
+    }
+  
   return totalAmount;
 }
