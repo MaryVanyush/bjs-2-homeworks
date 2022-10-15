f075aa9b0a53cda4e515047ae4b3b488371932fd
Maria Vanyushkina <m199178@mail.ru>
2022-10-15 18:47:27 +0300

    updated objects

--- 4.objects/main.js
+++ 4.objects/main.js
@@ -3,6 +3,7 @@ student1.setSubject("Algebra");
 student1.addMark(5);
 student1.addMark(4);
 student1.addMark(5);
+console.log(student1.getAverage());
 
 let student2 = new Student("Buzz", "female", 35);
 student2.setSubject("Geometry");
--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,35 @@
 function Student(name, gender, age) {
-    // Ваш код
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
 
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  if(this.marks === undefined){ 
+    this.marks = [mark];
+    } else {
+      this.marks.push(mark);
+    }
+}
+
+Student.prototype.addMarks = function (...marks) {
+        this.marks = [...marks];
+}
+
+
+Student.prototype.getAverage = function () {
+  let sum = this.marks.reduce((initialmark, mark) => initialmark + mark);
+  return sum/this.marks.length;
+}
+
+Student.prototype.exclude = function (reason) {
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
+}
+
